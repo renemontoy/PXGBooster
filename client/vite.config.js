a4312c42b65f517799e0b342dfb277ababc3e09c
Rene Montoy <rivan@pxg.com>
2025-08-07 15:30:39 -0700

    URL

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'dist' // esto asegura que Vite cree el directorio que Render espera
