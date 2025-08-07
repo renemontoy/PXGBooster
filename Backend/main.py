9790cdbd0f5bfdd55a9f21618dbff805a951adb7
Rene Montoy <rivan@pxg.com>
2025-08-07 15:16:42 -0700

    Origins

--- Backend/main.py
+++ Backend/main.py
@@ -7,12 +7,17 @@ import os
 app = FastAPI()
 
 # Obtén el dominio del frontend desde una variable de entorno (para desarrollo y producción)
-frontend_origin = os.getenv("FRONTEND_ORIGIN", "http://localhost:5173")
+#frontend_origin = os.getenv("FRONTEND_ORIGIN", "http://localhost:5173")
 
+
+origins = [
+    "https://pxgbooster.onrender.com",  # Tu frontend en Render
+    "http://localhost:5173",            # Para desarrollo local con Vite
+]
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[frontend_origin],
+    allow_origins=[origins],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
