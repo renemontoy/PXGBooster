54b7d9cbef06a137a2ac44716b7775bcf2c46207
Rene Montoy <rivan@pxg.com>
2026-05-21 07:14:29 -0700

    Ferrule

--- Backend/scripts/Ferrule.py
+++ Backend/scripts/Ferrule.py
@@ -609,8 +609,8 @@ async def Ferrule(
             df6['Max. Shipped Clubs'] = pd.to_numeric(df6['Max. Shipped Clubs'], errors='coerce')
 
             #AGREGAR PRIMER TIPWEIGHT
-            w = "T-G4IRONWGHT-SIL-5G"
-            q = 30
+            w = "T-G4IRONWGHT-SIL-9.5G"
+            q = 150
             dffiltro = df6.loc[df6["Max. Shipped Clubs"] == 1]
             qlineas= dffiltro.head(q).copy()
             qlineas['Inventory Identifier'] = w
@@ -621,8 +621,8 @@ async def Ferrule(
             df7 ['Shipment Nbr Count'] = 1
 
             #AGREGANDO SEGUNDO TIPWEIGHT
-            we = "T-G4IRONWGHT-SIL-6.5G"
-            qu = 30
+            we = "T-TIPWEIGHT-5G"
+            qu = 200
             dffiltro2 = df7.loc[df7["Max. Shipped Clubs"] == 1]
             qlineas2 = dffiltro2.head(qu).copy()
             qlineas2["Inventory Identifier"] = we
