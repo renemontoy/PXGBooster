e50d30cd3fc9a0080ab5483b5e0f3aceac5c7689
Rene Montoy <rivan@pxg.com>
2026-09-10 07:01:24 -0700

    Ferrule

--- Backend/scripts/Ferrule.py
+++ Backend/scripts/Ferrule.py
@@ -626,7 +626,7 @@ async def Ferrule(
             df7 ['Shipment Nbr Count'] = 1
 
             #AGREGANDO SEGUNDO TIPWEIGHT
-            we = "T-TIPWEIGHT-1G"
+            we = "T-GEN8IRWGT-SIL-6G"
             qu = 1500
             dffiltro2 = df7.loc[df7["Max. Shipped Clubs"] == 1]
             qlineas2 = dffiltro2.head(qu).copy()
